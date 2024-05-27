a07e82a686300294bfd891866bd61e7ce6b47278
Smit Visat <=>
2024-05-27 18:16:09 -0400

    commit 02_subtract.py

---
+++ 02_subtract.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    # Ask the user for two numbers
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    # Calculate the sum
+    sum = num1 - num2
+
+    # Print the sum
+    print("The sum of the two numbers is:", sum)
